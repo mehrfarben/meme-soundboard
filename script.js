011a07bcd95c4f1ace7a24d54c26d214937b94d3
Alim Karaca <alim.karaca2000@gmail.com>
2024-02-14 18:01:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = ["boing", "boom", "fail", "huh", "oof", "shocked"];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+
+        document.getElementById(sound).play();
+    });
+
+    document.body.appendChild(btn);
+});
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
